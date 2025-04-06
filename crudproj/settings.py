9fb56ec5900d3672c408a4e6ea4e67c25e69f9d8
Nani-avidi <raghavendraavidi2003@gmail.com>
2025-04-07 01:06:47 +0530

    Added python version for render Fixed new secretkey

--- crudproj/settings.py
+++ crudproj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
+SECRET_KEY = 'django-insecure-674-0x#1bx-oeur_dcmy4-2%-@n_vlfi!qbe-wj&5+-^oertvn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
